20a89ec9779fc8b6a1f4d3ecac88376fbc3e7779
kearmododragon <ckearney1992@gmail.com>
2023-03-08 21:33:13 +0100

    cards glow, v. important. Play button gives name, deal button returns bet + shuffled deck. code for dealing not working yet

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 let score = 100;
 let bet;
 let user;
-var dealerSum =0;
-var userSum =0;
-var hidden;
-var dealerAces =0;
-var userAces =0;
-var canHit = true;
+let dealerSum = 0;
+let userSum = 0;
+let hidden;
+let dealerAces = 0;
+let userAces = 0;
+let canHit = true;
 
 
 // User's name + starter money//
@@ -16,71 +16,70 @@ function enterName() {
   if (user != null) {
     document.getElementById("name").innerHTML =
       `Hello, ${user}! Sit down and let's play Blackjack!`;
-      document.getElementById("user").innerHTML =
+    document.getElementById("user").innerHTML =
       `${user}:`
     document.getElementById("currentScore").innerHTML =
       `Current Pot: $ ${score}`;
-      console.log(user)
+    console.log(user)
   }
 }
 // need to have "enter name" auto removed //
 
 //leave table button//
-/*function restartGame() {
-  for (var i = 0; i < board.length; i++) {
-    board[i] = 0
-  }}
-
-/*if (score > highScore) {
-
-}*/
-// reset board
-
-
-//practice code block /|\
-
+//function resetGame()
 // code for 52 card deck + shuffle functions//
 function buildDeck() {
-  var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-  var suits = ["Spade", "Diamond", "Heart", "Clubs"];
-  var deck = [];
+  let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let suits = ["Spade", "Diamond", "Heart", "Clubs"];
+  let deck = [];
 
-  for (var suitAllocater = 0; suitAllocater < 4; suitAllocater++) {
-    for (var rankAllocator = 0; rankAllocator < 13; rankAllocator++) {
+  for (let suitAllocater = 0; suitAllocater < 4; suitAllocater++) {
+    for (let rankAllocator = 0; rankAllocator < 13; rankAllocator++) {
       deck.push(ranks[rankAllocator] + suits[suitAllocater]);
     }
   }
   return deck;
 }
 
-function shuffleDeck(deck) {
-  for (var i = 0; i < 52; i++) {
-    var cardA = deck[i];
-    var randomIndex = Math.floor(Math.random() * 52);
+function shuffDeck(deck) {
+  for (let i = 0; i < 52; i++) {
+    let cardA = deck[i];
+    let randomIndex = Math.floor(Math.random() * 52);
     deck[i] = deck[randomIndex];
     deck[randomIndex] = cardA;
-    console.log(shuffledDeck);
   }
+  return deck
 }
-console.log(shuffledDeck)
-var shuffledDeck = buildDeck();
 
-//-------------------- deal button -------------------- //
+let builtDeck = buildDeck();
+let shuffledDeck = shuffDeck(builtDeck)
 
 function dealBet() {
   let bet = prompt("How much would you like to bet?", "$$$");
   if (bet <= score) {
     document.getElementById("bet").innerHTML =
       `Current bet: $ ${bet}`;
-        }
+  }
   console.log(bet)
   console.log(shuffledDeck)
-}
 
+
+window.onLoad = function () {
+startGame();
+}
+console.log(startGame)
+
+function startGame() {
+  hidden = deck.pop();
+  dealerSum += cardValue(hidden);
+  dealerAces += checkAce(hidden);
+  console.log(hidden);
+  console.log(dealerSum);
+}
 function cardValue(cardA) {
   let data = cardA.split("-");
   let value = data[0];
-  
+
   if (isNaN(value)) {
     if (value === "A") {
       return 11;
@@ -88,31 +87,19 @@ function cardValue(cardA) {
   }
   return parseInt(value);
 }
-function startGame() {
-  hidden = deck.pop();
-  dealerSum += getValue(hidden) 
-  dealerAces += checkAce(hidden);
-  console.log(hidden)
-  console.log(dealerSum)
-}
 function checkAce(cardA) {
-  if (cardA[0] === "A"){
-  return 1;
-} return 0;
+  if (cardA[0] === "A") {
+    return 1;
+  } 
+  return 0;
 }
-
-/*
-
-
-
-
-
+}
 // highest total counter //
 
 // bet counter //
 
-// time at the table // 
-let timer
+// time at the table //
+/*let timer
 let count = 0
 timer = setInterval(function () {
   console.log("timers are cool", count)
