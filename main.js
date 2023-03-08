073afb8f76d8452345d71c460ee7d4f9a8fe1577
kearmododragon <ckearney1992@gmail.com>
2023-03-08 22:29:04 +0100

    cards glow which is cute, no further in base code. Need to deal the cards.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Starter Money //
 let score = 100;
 let bet;
 let user;
@@ -9,8 +8,6 @@ let dealerAces = 0;
 let userAces = 0;
 let canHit = true;
 
-
-// User's name + starter money//
 function enterName() {
   let user = prompt("Hello! What is your name?", "Enter name");
   if (user != null) {
@@ -27,7 +24,6 @@ function enterName() {
 
 //leave table button//
 //function resetGame()
-// code for 52 card deck + shuffle functions//
 function buildDeck() {
   let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
   let suits = ["Spade", "Diamond", "Heart", "Clubs"];
@@ -48,12 +44,12 @@ function shuffDeck(deck) {
     deck[i] = deck[randomIndex];
     deck[randomIndex] = cardA;
   }
-  return deck
+  return deck;
 }
 
 let builtDeck = buildDeck();
 let shuffledDeck = shuffDeck(builtDeck)
-
+//-----------------DONT TOUCH ABOVE THIS LINE-----------------//
 function dealBet() {
   let bet = prompt("How much would you like to bet?", "$$$");
   if (bet <= score) {
@@ -62,61 +58,32 @@ function dealBet() {
   }
   console.log(bet)
   console.log(shuffledDeck)
-
-
-window.onLoad = function () {
-startGame();
 }
-console.log(startGame)
 
-function startGame() {
-  hidden = deck.pop();
-  dealerSum += cardValue(hidden);
-  dealerAces += checkAce(hidden);
-  console.log(hidden);
-  console.log(dealerSum);
+
+function dealCard() {
+  hidden = deck.shift();
+  dealerSum += getValue(hidden);
+dealerAceCount += checkAce
+console.log(hidden);
+console.log(dealerSum);
 }
-function cardValue(cardA) {
+function getValue(cardA) {
   let data = cardA.split("-");
   let value = data[0];
 
   if (isNaN(value)) {
     if (value === "A") {
       return 11;
-    } return 10;
+    }
+    return 10;
   }
   return parseInt(value);
 }
+
 function checkAce(cardA) {
-  if (cardA[0] === "A") {
+  if (card[0] === "A"){
     return 1;
-  } 
+  }
   return 0;
 }
-}
-// highest total counter //
-
-// bet counter //
-
-// time at the table //
-/*let timer
-let count = 0
-timer = setInterval(function () {
-  console.log("timers are cool", count)
-  count = count + 1
-  if (count > 10) {
-    clearInterval(timer)
-  }
-}, 2000)
-
-  // deal users + dlr cards //
-var dealerSum =0;
-var userSum =0;
-
-var dealerAces =0;
-var userAces =0;
-
-var hidden;
-*/
-
-// on event listener for Deal, initiate buildDeack();
